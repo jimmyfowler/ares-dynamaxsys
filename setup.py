6a7f5a91321497a31be55df8995b5c78dbf6f258
Karen Leung <karen.ym.leung@gmail.com>
2025-07-14 16:48:34 -0700

    updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="dynamaxsys",
-    version="0.0.5",
+    version="0.0.6",
     description="dynamical systems with jax",
     author="Karen Leung",
     author_email="kymleung@uw.edu",
