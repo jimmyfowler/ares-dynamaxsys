fixed body-inertial rotation in 4dof model

dynamaxsys/parafoil.py
@@ -196,8 +196,6 @@ class JannParafoil4DOF2(Dynamics):
             )  # down accel
 
             
-
-            ## ADDING THESE
             inertial_to_body = getInertialToBodyRotationMatrix(phi, theta, psi)
             body_to_inertial = inertial_to_body.T  # rotation matrix is orthogonal
 
@@ -206,20 +204,11 @@ class JannParafoil4DOF2(Dynamics):
 
             uvw_dot = jnp.array([u_dot, v_dot, w_dot])
 
-            pqr_to_euler_dot = jnp.array( 
-                [
-                    [1, jnp.sin(phi) * jnp.tan(theta), jnp.cos(phi) * jnp.tan(theta)],
-                    [0, jnp.cos(phi), -jnp.sin(phi)],
-                    [0, jnp.sin(phi) / jnp.cos(theta), jnp.cos(phi) / jnp.cos(theta)],
-                ]
-            )
-            euler_to_pqr_dot = pqr_to_euler_dot.T #TODO: FIX THIS SHI ITS WRONG
-
-            # euler angle dynamics (from body rates)
             euler_dot = jnp.array([phi_dot, theta_dot, psi_dot])
 
-            # body frame rotational dynamics
-            pqr_dot = euler_to_pqr_dot @ euler_dot
+            #get body frame angular velocities from euler rates
+            pqr_from_euler_dot = getBodyRatesFromEulerRates(phi, theta)
+            pqr_dot = pqr_from_euler_dot @ euler_dot
 
             return jnp.concatenate([xyz_dot, uvw_dot, euler_dot, pqr_dot])
 
@@ -264,17 +253,10 @@ class JannParafoil3DOF(Dynamics):
             psi_dot = r
 
             # no roll or pitch angle
-            phi_dot = 0.0
-            theta_dot = 0.0
-            p_dot = 0.0
-            q_dot = 0.0
             phi = 0.0
             theta = 0.0
 
             # constant body velocities
-            u_dot = 0.0
-            v_dot = 0.0
-            w_dot = 0.0
             u = self.u_0
             v = 0.0
             w = self.w_0
@@ -285,16 +267,15 @@ class JannParafoil3DOF(Dynamics):
             # pqr_from_euler_dot = getBodyRatesFromEulerRates(phi, theta)
 
             # inertial-frame velocity
-            xyz_dot = body_to_inertial @ jnp.array([u, v, w]) + disturbance
-
-            uvw_dot = jnp.array([u_dot, v_dot, w_dot])
-            euler_dot = jnp.array([phi_dot, theta_dot, psi_dot])
-            pqr_dot = jnp.array([p_dot, q_dot, r_dot])  # only r_dot nonzero
+            xyz_ned_dot = body_to_inertial @ jnp.array([u, v, w]) + disturbance
+            uvw_dot = jnp.array([0, 0, 0])
+            euler_dot = jnp.array([0, 0, psi_dot])
+            pqr_dot = jnp.array([0, 0, r_dot])  # only r_dot nonzero
 
             # # body frame rotational dynamics
             # pqr = pqr_from_euler_dot @ euler_dot
 
-            return jnp.concatenate([xyz_dot, uvw_dot, euler_dot, pqr_dot])
+            return jnp.concatenate([xyz_ned_dot, uvw_dot, euler_dot, pqr_dot])
 
         # initialize super class Dynamics object
         super().__init__(dynamics_func, self.state_dim, self.control_dim)